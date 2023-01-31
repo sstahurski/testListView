6cef60008ca6cdf6f3f1e13ae67edaedc70bb71d
Scott Stahurski <scott@Scotts-Mac-Studio.local>
2023-01-31 10:49:52 -0700

    initial CI

---
+++ testNoShell/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace testNoShell;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new NavigationPage(new MainPage()); 
+	}
+}
+
---
+++ testNoShell/MainPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace testNoShell
+{
+    public class Person 
+    {
+        public string Name { get; set; }
+
+        public Person()
+        {
+        }
+    }
+
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private ObservableCollection<Person> persons = new ObservableCollection<Person>();
+        public ObservableCollection<Person> Persons
+        {
+            get { return persons; }
+            set
+            {
+                persons = value;
+            }
+        }
+
+        public MainPageViewModel()
+        {
+
+            string[] names = { "Billy", "Jim", "Joe", "Bob", "Scott", "Keith", "Cory", "Bode", "Don", "Richard", "Tyler", "Mason", "Antony", "Harold", "Lowell", "Loyd", "Morton", "Jesse" };
+
+            foreach (string name in names) {
+                persons.Add(new Person() { Name = name });
+            }
+
+            foreach (string name in names)
+            {
+                persons.Add(new Person() { Name = name });
+            }
+        }
+
+
+    }
+}
+
